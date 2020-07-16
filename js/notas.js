767d9a341f60e8852eaa42f72a81110e7b9f6290
Daniel <victor.rodriguez@sofka.com.co>
2020-07-16 17:28:41 -0500

    ejercicio de notas

--- js/notas.js
+++ js/notas.js
@@ -1,23 +1,25 @@
-'use strict'
+"use strict";
 
-
-var cantidadNotas = parseInt(prompt("Ingrese la notas que serán evaluadas", 0));
-var notas = new Array(cantidadNotas);
 var porcentaje = new Array(3);
 var total = 0;
 
+do {
+  for (let i = 0; i < porcentaje.length; i++) {
+    porcentaje[i] = parseInt(
+      prompt("Ingrese el porcentaje del corte " + (i + 1), 0)
+    );
+  }
+  porcentaje.forEach((element) => {
+    total += element;
+  });
+} while (total != 100);
 
-for (let i = 0; i < cantidadNotas; i++) {
+porcentaje.forEach((element, index) => {
+  var cantidadNotas = parseInt(prompt("Ingrese la cantidad de notas que serán evaluadas para el corte: " + (index + 1), 0));
+
+  var notas = new Array(cantidadNotas);
+  for (let i = 0; i < cantidadNotas; i++) {
     notas[i] = parseInt(prompt("Ingrese la nota # " + (i + 1), 0));
     document.write("<h3>" + notas[i] + "<h3/>");
-}
-
-do {
-    for (let i = 0; i == 3; i++) {
-        porcentaje[i] = parseInt(prompt("Ingrese el porcentaje de la nota " + (i + 1), 0));
-    }
-    porcentaje.forEach(element => {
-        total += element;
-    });
-
-} while (total != 100);
+  }
+});
