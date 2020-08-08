675312fbf97cec98c9d836d23ba9799625d4085d
Daniel <victor.rodriguez@sofka.com.co>
2020-08-08 14:38:23 -0500

    se realiza juego

--- js/arrays.js
+++ js/arrays.js
@@ -1,17 +1,29 @@
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']
+
+const numbers = [1, 5, 10, 15, 65, 0]
 
 //obtener indice de un elemento
-words.indexOf()
+words.indexOf(2)
 
-//Encontrar un elemento
-words.find(callback)
+//Encontrar un elemento, devuelve el valor del primer elemento del array que cumple la función de prueba proporcionada.
+numbers.find(valor => valor > 16)
+
+//Encontrar un elemento, devuelve el indice del primer elemento del array que cumple la función de prueba proporcionada,
 words.findIndex(callback)
 
-//Algún elemnto
-words.some(callback)
+//Valida si algún elemento del array
+const result = words.some(word => word.length > 6) //retorna boolean
 
-//Todos los elementos
-words.every(callback)
+//Valida si todos los elementos del array
+numbers.every(valor => valor > 10) //retorna boolean
 
 //Filtrar elemento
-const result = words.filter(word => word.length > 6);
+const result = words.filter(word => word.length > 6) //retorna las palabras
+
+//Itera un array
+const newNumbers = numbers.map(x => {
+    return x * 2
+})
+
+//Itera el array y suma cada uno
+var g = numbers.reduce((accumulator, currentValue) => accumulator + currentValue)
--- js/arrows.js
+++ js/arrows.js
@@ -11,3 +11,9 @@ var datos = {
 
 const esMayorDeEdad = dato => dato.edad > MAYOR_EDAD ? `Mayor de edad: ${dato.edad}` : `Menor de edad: ${dato.edad}`
 const estudia = ({ estudiando, nombre }) => estudiando ? `${nombre} está aprendiendo` : `${nombre} es un vago`
+
+
+const sumaEdad = dato => ({
+    ...dato,
+    edad: dato.edad + 1
+})
