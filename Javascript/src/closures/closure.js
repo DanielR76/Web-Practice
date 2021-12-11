f84ccf48650065098b0130486c0c053dcb54cb2f
Daniel <danielr2397@gmail.com>
2021-12-11 13:17:08 -0500

    Order folder & create closure example

---
+++ Javascript/src/closures/closure.js
@@ -0,0 +1,25 @@
+//Example no closure
+const noClosure = (coins) => {
+	let saveCoins = 0;
+	saveCoins += coins;
+	console.log(saveCoins);
+};
+
+noClosure(5);
+noClosure(4);
+
+//Example closure
+const closure = () => {
+	let saveCoins = 0;
+	const countCoints = (coins) => {
+		saveCoins += coins;
+		console.log(saveCoins);
+	};
+	return countCoints;
+};
+
+let newValue = closure();
+
+newValue(5);
+newValue(10);
+newValue(15);
