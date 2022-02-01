043ec00f4b12cc69e050b206166f91a75510efc5
Daniel Rodriguez <danielr2397@gmail.com>
2022-02-01 09:10:45 -0500

    Implement useCallback useMemo useRef

--- ReactJs/src/Components/Characters.jsx
+++ ReactJs/src/Components/Characters.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import {
+	useState,
+	useEffect,
+	useReducer,
+	useRef,
+	useMemo,
+	useCallback,
+} from "react";
+import Search from "./Search";
+
 import classNames from "classnames";
 import "../assest/css/characters.css";
-import { useReducer } from "react";
 
 const initialState = {
 	favorites: [],
@@ -23,12 +31,9 @@ const favoriteReducer = (state, action) => {
 const Characters = ({ darkMode }) => {
 	const [characters, setCharacters] = useState([]);
 	const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
-
+	const [search, setSearch] = useState("");
 	const darkClass = classNames({ dark: darkMode });
-
-	const handleClick = (fav) => {
-		dispatch({ type: "ADD_FAVORITE", payload: fav });
-	};
+	const refSearch = useRef(null);
 
 	useEffect(() => {
 		fetch("https://rickandmortyapi.com/api/character")
@@ -36,24 +41,44 @@ const Characters = ({ darkMode }) => {
 			.then((data) => setCharacters(data.results));
 	}, []);
 
+	const handleClick = (fav) => {
+		dispatch({ type: "ADD_FAVORITE", payload: fav });
+	};
+
+	const handleSearch = useCallback(() => {
+		setSearch(refSearch.current.value);
+	}, [refSearch]);
+
+	const filteredUsers = useMemo(() => {
+		return characters.filter((element) =>
+			element.name.toLowerCase().includes(search?.toLowerCase())
+		);
+	}, [characters, search]);
+
 	return (
 		<div className="card__content">
-			<ul>
+			<ul className="card__list--favorite">
 				{favorites.favorites.map((element, index) => (
 					<li key={index}>{element.name}</li>
 				))}
 			</ul>
-
-			{characters?.map((element, index) => (
-				<span
-					className={`card__unique ${darkClass}`}
-					key={index}
-					type="button"
-					onClick={() => handleClick(element)}
-				>
-					{element.name}
-				</span>
-			))}
+			<Search
+				search={search}
+				refSearch={refSearch}
+				handleSearch={handleSearch}
+			/>
+			<span>
+				{filteredUsers?.map((element, index) => (
+					<span
+						className={`card__unique ${darkClass}`}
+						key={index}
+						type="button"
+						onClick={() => handleClick(element)}
+					>
+						{element.name}
+					</span>
+				))}
+			</span>
 		</div>
 	);
 };
---
+++ ReactJs/src/Components/Search.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Search = ({ search, refSearch, handleSearch }) => {
+	return (
+		<div>
+			<input
+				type="text"
+				value={search}
+				ref={refSearch}
+				onChange={handleSearch}
+			/>
+		</div>
+	);
+};
+
+export default Search;
